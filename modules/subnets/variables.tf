c1050227dfa50f83f93457b1e7f1692c8a3e4f41
Mithun S <40950581+mithuns1986@users.noreply.github.com>
2025-08-20 17:39:20 +0530

    Update variables.tf

--- modules/subnets/variables.tf
+++ modules/subnets/variables.tf
@@ -1,7 +1,7 @@
 variable "name_prefix" { type = string }
 variable "vpc_id" { type = string }
 variable "azs" { type = list(string) }
-variable "public_subnet_cidrs" { type = list(string), default = [] }
-variable "private_subnet_cidrs" { type = list(string), default = [] }
-variable "map_public_ip_on_launch" { type = bool, default = true }
-variable "tags" { type = map(string), default = {} }
+variable "public_subnet_cidrs" { type = list(string) }
+variable "private_subnet_cidrs" { type = list(string) }
+variable "map_public_ip_on_launch" { type = bool }
+variable "tags" { type = map(string) }
