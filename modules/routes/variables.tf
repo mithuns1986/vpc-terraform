790802cfdb535886e6547a3d790bd09b7287969b
Mithun S <40950581+mithuns1986@users.noreply.github.com>
2025-08-20 17:38:50 +0530

    Update variables.tf

--- modules/routes/variables.tf
+++ modules/routes/variables.tf
@@ -1,7 +1,7 @@
 variable "vpc_id" { type = string }
 variable "igw_id" { type = string }
-variable "public_subnet_ids" { type = list(string), default = [] }
-variable "private_subnet_ids" { type = list(string), default = [] }
-variable "create_public_rt" { type = bool, default = true }
-variable "create_private_rt" { type = bool, default = true }
-variable "tags" { type = map(string), default = {} }
+variable "public_subnet_ids" { type = list(string) }
+variable "private_subnet_ids" { type = list(string) }
+variable "create_public_rt" { type = bool }
+variable "create_private_rt" { type = bool }
+variable "tags" { type = map(string) }
